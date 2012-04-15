3378c98ada2c4f136c3362404071742b314595c5
Daniel Ribeiro <danielr@twitch.tv>
2012-04-15 13:57:34 -0700

    run tests script

--- create_constants.sh
+++ create_constants.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -cp src:bin/clojure.jar:../bin/minecraft:../jars/bin/jinput.jar:../jars/bin/lwjgl.jar:../jars/bin/lwjgl_util.jar:../jars/bin/md5s:../jars/bin/minecraft.jar:../jars/bin/minecraft.jar.bak:../jars/bin/natives:../jars/bin/version  clojure.main src/create_constants.clj
+java -cp .:bin/clojure-1.3.0.jar:../bin/minecraft:../jars/bin/jinput.jar:../jars/bin/lwjgl.jar:../jars/bin/lwjgl_util.jar:../jars/bin/md5s:../jars/bin/minecraft.jar:../jars/bin/minecraft.jar.bak:../jars/bin/natives:../jars/bin/version  clojure.main create_constants.clj
---
+++ run_tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -cp .:bin/clojure-1.3.0.jar clojure.main recipe_dsl_test.clj
