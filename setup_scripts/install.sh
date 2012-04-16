9ef0dbd2a9654e4a13a22bd10e13c11490559834
Daniel Ribeiro <danielr@twitch.tv>
2012-04-15 17:38:38 -0700

    missing var on install.sh

--- setup_scripts/install.sh
+++ setup_scripts/install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
+if [ 'Linux' = `uname -s` ]
+then minecraft_path="$HOME/.minecraft"
+else  
+  minecraft_path="$HOME/Library/Application Support/minecraft"
+fi
+
 echo "-> Setting up conjcraft's environment"
 cd ~
 wget https://github.com/downloads/danielribeiro/conjcraft/conjcraft-0.1.zip
